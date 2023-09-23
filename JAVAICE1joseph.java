6ac72ef46964c249d0f7e7b0a2baace7cfc80668
Prince Bepo <86449570+royalghostbepo@users.noreply.github.com>
2023-09-23 13:26:02 -0400

    Add files via upload

---
+++ JAVAICE1joseph.java
@@ -0,0 +1,20 @@
+public class WelcomeProgram {
+    public static void main(String[] args) {
+        // Displaying a welcome message with name and banner id
+        System.out.println("Welcome! My name is Joseph Bepo and my banner id is 100901775.");
+
+        // Displaying a favorite quote with its source
+        System.out.println("One of my favorite quotes is: 'Your quote here' - Work hard and in silence...");
+
+        // Asking the user for decimal numeric input
+        System.out.print("Please enter a decimal numeric value: ");
+        double userInput = Double.parseDouble(System.console().readLine());
+
+        // Evaluating if the user input is within acceptable boundaries
+        if (userInput >= 50 && userInput <= 999) {
+            System.out.println("The number you entered is " + userInput + " and it is within acceptable boundaries (50 and 999).");
+        } else {
+            System.out.println("Alert, the number you entered is " + userInput + " and it is outside acceptable boundaries (50 and 999).");
+        }
+    }
+}
